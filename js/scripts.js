b9cabbcf1e5cf85bfcd65fdf83c29499d399c179
DavidKibetRono <david.rono@student.moringaschool.com>
2021-11-21 13:36:34 +0300

    add hover event on portfolio sections

--- js/scripts.js
+++ js/scripts.js
@@ -35,4 +35,84 @@ $(document).ready(function(){
     });
 
 
+    //work4
+    $("#work4").on({
+        mouseover: function() {
+            $("#fadeImage4").show();
+        },
+        mouseout: function() {
+            $("#fadeImage4").hide();
+        }
+    })
+
+    //work3
+    $("#work3").on({
+        mouseover: function() {
+            $("#fadeImage3").show();
+        },
+        mouseout: function() {
+            $("#fadeImage3").hide();
+        }
+    })
+
+    //work2
+    $("#work2").on({
+        mouseover: function() {
+            $("#fadeImage2").show();
+        },
+        mouseout: function() {
+            $("#fadeImage2").hide();
+        }
+    })
+
+    //work1
+    $("#work1").on({
+        mouseover: function() {
+            $("#fadeImage1").show();
+        },
+        mouseout: function() {
+            $("#fadeImage1").hide();
+        }
+    })
+
+    //work5
+    $("#work5").on({
+        mouseover: function() {
+            $("#fadeImage5").show();
+        },
+        mouseout: function() {
+            $("#fadeImage5").hide();
+        }
+    })
+
+    //work6
+    $("#work6").on({
+        mouseover: function() {
+            $("#fadeImage6").show();
+        },
+        mouseout: function() {
+            $("#fadeImage6").hide();
+        }
+    })
+
+    //work7
+    $("#work7").on({
+        mouseover: function() {
+            $("#fadeImage7").show();
+        },
+        mouseout: function() {
+            $("#fadeImage7").hide();
+        }
+    })
+
+    //work8
+    $("#work8").on({
+        mouseover: function() {
+            $("#fadeImage8").show();
+        },
+        mouseout: function() {
+            $("#fadeImage8").hide();
+        }
+    })
+    
 })
